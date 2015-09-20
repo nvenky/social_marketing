1232787752526ace8d955e77ce986237dae7e185
Yik Surn Chong <yiksurn.chong@gmail.com>
2015-09-20 14:33:56 +1000

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,6 +44,11 @@ Fabricator(:campaign) do
     description { Faker::Lorem.paragraph[0..250]}
 end
 
+david = User.create!({email: 'david@ibtuition.com', password: 'testtest', password_confirmation: 'testtest', usertype: 'influencer'})
+Influencer.create!({first_name: 'David', last_name: 'Gai', description: 'lorem ipsum', youtube_followers: 12127, minimum_price: 100, user: david})
+sreejay = User.create!({email: 'sreejay@gmail.com', password: 'testtest', password_confirmation: 'testtest', usertype: 'influencer'})
+Influencer.create!({first_name: 'Sreejay', last_name: 'Santosh', description: 'lorem ipsum', instagram_followers: 2848, minimum_price: 100, user: sreejay})
+
 umate = User.create!({email: 'sam.sweeny@umate.com.au', password: 'testtest', password_confirmation: 'testtest', usertype: 'advertiser'})
 Advertiser.create!({company_name: 'UMate', website: 'www.umate.com.au', contact_person: 'sam.sweeny@umate.com.au', user: umate})
 ibblueprint = User.create!({email: 'sam.sweeny@ibblueprint.com', password: 'testtest', password_confirmation: 'testtest', usertype: 'advertiser'})
@@ -51,10 +56,6 @@ Advertiser.create!({company_name: 'IB Blue Print', website: 'ibblueprint.com', c
 nutrition = User.create!({email: 'van@realedgenutrition.com', password: 'testtest', password_confirmation: 'testtest', usertype: 'advertiser'})
 Advertiser.create!({company_name: 'Real Edge Nutrition', website: 'realedgenutrition.com.au', contact_person: 'van@realedgenutrition.com', user: nutrition})
 
-david = User.create!({email: 'david@ibtuition.com', password: 'testtest', password_confirmation: 'testtest', usertype: 'influencer'})
-Influencer.create!({first_name: 'David', last_name: 'Gai', description: 'lorem ipsum', youtube_followers: 12127, minimum_price: 100, user: david})
-sreejay = User.create!({email: 'sreejay@gmail.com', password: 'testtest', password_confirmation: 'testtest', usertype: 'influencer'})
-Influencer.create!({first_name: 'Sreejay', last_name: 'Santosh', description: 'lorem ipsum', instagram_followers: 2848, minimum_price: 100, user: sreejay})
 
 100.times { Fabricate(:influencer) }
 
